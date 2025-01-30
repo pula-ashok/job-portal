c9e9d667e6b6170069bf1c104b2f85c569a52b54
pula-ashok <ashokpula0307@gmail.com>
2025-01-30 16:53:51 +0530

    user button intregrated in navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { assets } from './../assets/assets';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const {openSignIn}=useClerk()
+  const {user}=useUser()
   return (
     <div className='shadow py-4'>
         <div className='container flex justify-between items-center px-4 2xl:px-20 mx-auto'>
             <img src={assets.logo} alt='logo'/>
-            <div className='flex gap-4 max-sm:text-xs'>
+            {
+              user?
+              <div className='flex items-center gap-3'>
+                <Link to={'/applications'}>Applied Jobs</Link>
+                <p>|</p>
+                <p>Hi, {user.firstName+" "+user.lastName}</p>
+                <UserButton/>
+              </div>:
+               <div className='flex gap-4 max-sm:text-xs'>
                 <button className='text-gray-600'>Recruiter Login</button>
-                <button className='bg-blue-600 text-white px-6 sm:px-8 py-2 rounded-full'>Login</button>
-            </div>
+                <button className='bg-blue-600 text-white px-6 sm:px-8 py-2 rounded-full' onClick={e=>openSignIn()}>Login</button>
+              </div>
+            }
+           
         </div>
     </div>
   )
