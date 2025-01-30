f8d0d2ba956a00367652d0d5a1ff65b947cce8e3
pula-ashok <ashokpula0307@gmail.com>
2025-01-30 20:42:46 +0530

    adding filter for mobile view in sidebar

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -2,9 +2,11 @@ import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
 import { assets, JobCategories, JobLocations } from '../assets/assets';
 import JobCard from './JobCard';
+import { useState } from 'react';
 
 const JobListing = () => {
     const {setSearchedFilter,setIsSearched,isSearched,searchedFilter,jobs}=useContext(AppContext)
+    const [showFilter, setShowFilter] = useState(true)
   return (
     <div className="container 2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8">
       {/* sidebar */}
@@ -30,8 +32,9 @@ const JobListing = () => {
               </div>
             </>
           )}
+        <button onClick={e=>setShowFilter(prev=>!prev)} className='px-6 py-1.5 rounded border border-gray-400 lg:hidden'>{showFilter? "Close":"Filters"}</button>          
           {/* category filter  */}
-          <div className='max-lg:hidden'>
+          <div className={showFilter?"":"max-lg:hidden"}>
             <h4 className='font-medium text-lg py-4'>Search by Categories</h4>
             <ul className='space-y-4 text-gray-600'>
               {JobCategories.map((category,index)=><li key={index} className='flex items-center gap-3'>
@@ -41,7 +44,7 @@ const JobListing = () => {
             </ul>
           </div>
           {/* location filter  */}
-          <div className='max-lg:hidden'>
+          <div className={showFilter?"":"max-lg:hidden"}>
             <h4 className='font-medium text-lg py-4 pt-14'>Search by Location</h4>
             <ul className='space-y-4 text-gray-600'>
               {JobLocations.map((location,index)=><li key={index} className='flex items-center gap-3'>
