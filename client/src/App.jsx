17e7ce7bfebc90560409d9957becd780c1b51688
pula-ashok <ashokpula0307@gmail.com>
2025-01-30 16:15:40 +0530

    react router dom setup done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Applications from './pages/Applications'
+import ApplyJob from './pages/ApplyJob'
 
 const App = () => {
   return (
-    <div className='bg-red-200'>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/applications' element={<Applications/>} />
+        <Route path='/apply-job/:id' element={<ApplyJob/>} />
+      </Routes>
+    </div>
   )
 }
 
