df0651be3137d9a384f4332b9037919d718b24ee
pula-ashok <ashokpula0307@gmail.com>
2025-02-02 14:46:01 +0530

    sentry installed and configured sdk

---
+++ server/config/instrument.js
@@ -0,0 +1,29 @@
+// Import with `import * as Sentry from "@sentry/node"` if you are using ESM
+// const Sentry = require("@sentry/node");
+import * as Sentry from "@sentry/node"
+// const { nodeProfilingIntegration } = require("@sentry/profiling-node");
+import {nodeProfilingIntegration} from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://8eb6292d716e791824e84ac70e6aaa06@o4508748796002304.ingest.us.sentry.io/4508748800393216",
+  integrations: [
+    nodeProfilingIntegration(),
+    Sentry.mongooseIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+});
+// Manually call startProfiler and stopProfiler
+// to profile the code in between
+Sentry.profiler.startProfiler();
+
+// Starts a transaction that will also be profiled
+Sentry.startSpan({
+  name: "My First Transaction",
+}, () => {
+  // the code executing inside the transaction will be wrapped in a span and profiled
+});
+
+// Calls to stopProfiling are optional - if you don't stop the profiler, it will keep profiling
+// your application until the process exits or stopProfiling is called.
+Sentry.profiler.stopProfiler();
