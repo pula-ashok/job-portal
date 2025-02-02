93481e7ca54ee337264506dcee37dab0dc6df422
pula-ashok <ashokpula0307@gmail.com>
2025-02-02 15:26:36 +0530

    created user model and webhook api

---
+++ server/controllers/webhooks.js
@@ -0,0 +1,54 @@
+import Webhook from 'svix'
+import User from '../models/User';
+
+//api controller function to manage clerk user with database
+
+export const webhooks=async(req,res)=>{
+    try {
+        // create a svix instance with cleark webhook secret 
+        const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        //verify headers
+        await whook.verify(req.body,{
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"],
+        })
+        //getting data from the request body
+        const {data,type}=req.body;
+        //switch case for different events
+        switch(type){
+            case "user.created":
+                //create user in database
+                const userData={
+                    _id:data.id,
+                    email:data.email_addresse[0].email,
+                    name:data.first_name+" "+data.last_name,
+                    image:data.image_url,
+                    resume:"",
+                }
+                await User.create(userData)
+                res.json({})
+                break;
+            case "user.updated":
+                //update user in database
+                const userDataUpdated={
+                    email:data.email_addresse[0].email,
+                    name:data.first_name+" "+data.last_name,
+                    image:data.image_url,
+                }
+                await User.findByIdAndUpdate(data.id,userUpdatedData)
+                res.json({})
+                break;
+            case "user.deleted":
+                //delete user in database
+                await User.findByIdAndDelete(data.id)
+                res.json({})
+                break;
+            default :
+                break
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Webhook error"})
+    }
+}
