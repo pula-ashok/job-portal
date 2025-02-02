1a64a317702a0898e504f0ac968ade4f78ca3c74
pula-ashok <ashokpula0307@gmail.com>
2025-02-02 23:39:19 +0530

    cloudinary setup done

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ import connectDB from './config/db.js';
 import * as Sentry from "@sentry/node"
 import { webhooks } from './controllers/webhooks.js';
 import companyRouter from "./routes/companyRoutes.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 //initialize express app
 const app = express();
 
 //connect to database
 await connectDB()
+await connectCloudinary()
 
 //middleware
 app.use(cors());
