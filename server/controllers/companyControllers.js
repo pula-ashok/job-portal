8036ee3ac042b492dc176ddab469b13583825b2f
pula-ashok <ashokpula0307@gmail.com>
2025-02-02 23:25:59 +0530

    started register company api

--- server/controllers/companyControllers.js
+++ server/controllers/companyControllers.js
@@ -1,7 +1,23 @@
+import bcrypt from "bcrypt"
+import Company from './../models/Company.js';
 
 //register a new company
-export const registerCompany=(req,res)=>{
-
+export const registerCompany=async(req,res)=>{
+    const {name,email,password} = req.body;
+    const imageFile = req.file;
+    if(!name || !email || !password || !imageFile){
+        return res.json({success:false,message:"Missing details"})
+    }
+    try {
+        const companyExists =  await Company.findOne({email});
+        if(companyExists){
+            return res.json({success:false,message:"Company already registered"})
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+    } catch (error) {
+        
+    }
 }
 
 //company login
