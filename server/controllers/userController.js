5bd38f6d6132e6978c4e07409d58716dad44f88f
pula-ashok <ashokpula0307@gmail.com>
2025-02-05 10:28:15 +0530

    get user data api completed

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,17 @@
 
+import User from './../models/User.js';
 // get user data
-export const getUserData=(req,res)=>{
-    
+export const getUserData=async(req,res)=>{
+        const userId = req?.auth?.userId;
+        try {
+            const user = await User.findById(userId);
+            if(!user){
+                return res.json({success:false,message:"User not found"});
+            }
+            res.json({success:true,user})
+        } catch (error) {
+            res.json({success:false,message:error.message})
+        }
 }
 
 //apply for a job
